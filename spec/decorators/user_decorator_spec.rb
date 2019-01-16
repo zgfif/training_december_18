6434a2ae33b7dd2cec1397d07de0bf19e46a1f3e
Alexander Tkachuk <mail@tksasha.me>
2019-01-16 18:48:23 +0200

    tiny refactored UserDecorator

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,17 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe UserDecorator do
-  subject { user.decorate }
-
-  let(:user) do
-    stub_model User,
-      id: 13,
-      email: 'pasha@gmail.com',
-      created_at: '2019-01-15',
-      updated_at: '2019-01-16'
+  let :user do
+    stub_model User, id: 13, email: 'pasha@gmail.com'
   end
 
-  its(:as_json) do
+  subject { user.decorate }
+
+  its :as_json do
     should eq id: 13, email: 'pasha@gmail.com'
   end
 end
