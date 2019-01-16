c76407e7c333e15433fac9ae7a6a7ae680e5b373
Alexander Tkachuk <mail@tksasha.me>
2019-01-16 18:45:15 +0200

    tiny refactored AuthTokenDecorator

--- spec/decorators/auth_token_decorator_spec.rb
+++ spec/decorators/auth_token_decorator_spec.rb
@@ -1,25 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe AuthTokenDecorator do
+  let :user do
+    stub_model User, id: 13, email: 'pasha@gmail.com'
+  end
+
+  let :auth_token do
+    stub_model AuthToken, id: 11, user: user
+  end
+
   subject { auth_token.decorate }
 
-  let(:user) do
-    stub_model User,
-      { id: 13,
-      email: 'pasha@gmail.com',
-      created_at: '2019-01-15',
-      updated_at: '2019-01-16' }
-  end
-
-  let(:auth_token) do
-    stub_model AuthToken,
-      id: 11,
-      user: user,
-      created_at: '2019-01-16',
-      updated_at: '2019-01-17'
-  end
-
-  its(:as_json) do
+  its :as_json do
     should eq auth_token: 11,  user: { id: 13, email: 'pasha@gmail.com' }
   end
 end
