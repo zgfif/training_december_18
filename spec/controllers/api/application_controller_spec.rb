2dd1df1552b30b72498ff080cb04e0c92991e252
zgfif <zihzag@gmail.com>
2019-01-21 08:35:56 +0200

    #3: add specs

--- spec/controllers/api/application_controller_spec.rb
+++ spec/controllers/api/application_controller_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Api::ApplicationController, type: :controller do
   it { should be_an ActsAsAuthorizedController }
+  it { should be_an ActsAsAuthenticatedController }
 end
