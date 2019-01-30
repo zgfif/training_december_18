9ae2a0873b944e3671060e0131a943ccf2df791e
zgfif <zihzag@gmail.com>
2019-01-30 07:31:34 +0200

    #4: add policies specs

---
+++ spec/policies/api/session_policy_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Api::SessionPolicy, type: :policy do
+  subject { described_class }
+
+  let(:record) { nil }
+
+  permissions :create? do
+
+    context do
+      let(:user) { nil }
+
+      it { should permit user, record }
+    end
+
+    context do
+      let(:user) { stub_model User, id: nil, email: nil }
+
+      it { should_not permit user, record }
+    end
+  end
+
+  permissions :destroy? do
+    context do
+      let(:user) { stub_model User, id: nil, email: nil }
+
+      it { should permit user, record }
+    end
+
+    context do
+      let(:user) { nil }
+
+      it { should_not permit user, record }
+    end
+  end
+end
