db0b16c09ac6fdd70fae24a24ffd16f96b3339b6
zgfif <zihzag@gmail.com>
2019-01-31 15:28:58 +0200

    #7: add asceding order

--- app/controllers/api/authors_controller.rb
+++ app/controllers/api/authors_controller.rb
@@ -1,19 +1,14 @@
 module Api
   class AuthorsController < ApplicationController
-  before_action :build_collection
     def index
-      collection
-      # p 'aaaaa'
-      # collection.first
-      # render json: collection, status: :ok
-      # render json: collection if collection
+      build_collection
     end
 
     private
     attr_reader :collection
 
     def build_collection
-      @collection = Author.all
+      @collection = Author.all.order(:name)
     end
   end
 end
