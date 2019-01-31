1b5f74d553e7475163ef53848e4dc274604d9431
zgfif <zihzag@gmail.com>
2019-01-31 19:32:31 +0200

    #7: add spec for authors_controller

---
+++ spec/controllers/api/authors_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Api::AuthorsController, type: :controller do
+  it { should be_an Api::ApplicationController }
+
+  describe '#collection' do
+    before { subject.instance_variable_set :@collection, :collection }
+
+    its(:collection) { should eq :collection }
+  end
+
+  describe '#build_collection' do
+    before do
+      expect(Author).to receive(:all) do
+        double.tap do |a|
+          expect(a).to receive(:order).with(:name).and_return(:collection)
+        end
+      end
+    end
+    before { subject.send :build_collection }
+
+    its(:collection) { should eq :collection }
+  end
+
+  describe '#index.json' do
+    before { expect(subject).to receive(:authenticate!).and_return(true) }
+
+    before { expect(subject).to receive(:build_collection) }
+
+    before { get :index, params: {}, format: :json }
+
+    it { should render_template(:index).with_status(200) }
+  end
+end
