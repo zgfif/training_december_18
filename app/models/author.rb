861b12f9d88d63f71c0bc70ca1103225c43685ba
zgfif <zihzag@gmail.com>
2019-01-31 23:29:45 +0200

    #9: add validation of uniqueness of author name

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,6 @@ class Author < Person
   delegate :as_json, to: :decorate
 
   validates :name, presence: true
+
+  validates :name, uniqueness: { case_sensitive: false }
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Author, type: :model do
   it { should delegate_method(:as_json).to(:decorate) }
 
   it { should validate_presence_of :name }
+
+  it { should validate_uniqueness_of(:name).case_insensitive }
 end
