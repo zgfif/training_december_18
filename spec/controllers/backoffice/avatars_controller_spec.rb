51927485e65e83364651ad59e5f63dc42b5dff9e
zgfif <zihzag@gmail.com>
2019-02-10 15:46:52 +0200

    #12: add specs for avatars_controller

--- spec/controllers/backoffice/avatars_controller_spec.rb
+++ spec/controllers/backoffice/avatars_controller_spec.rb
@@ -10,13 +10,35 @@ RSpec.describe Backoffice::AvatarsController, type: :controller do
   end
 
   describe '#build_resource' do
-    let(:params) { acp id: nil, avatar: nil }
+    let(:resource_params) { acp id: nil, avatar: nil }
 
-    before { expect(subject).to receive(:params).and_return(params) }
+    let(:author) { double }
 
-    before { expect(Author).to receive(:find).with(params[:id]).and_return(:resource) }
+    before { expect(subject).to receive(:resource_params).and_return(resource_params) }
+
+    before {expect(subject).to receive(:author).and_return(author) }
+
+    before { expect(Backoffice::Avatar).to receive(:new).with(author, resource_params[:avatar]).and_return(:resource) }
 
     its(:build_resource) { should eq :resource }
+  end
+
+  describe '#author' do
+    context do
+      before { subject.instance_variable_set(:@author, :author) }
+
+      its(:author) { should eq :author }
+    end
+
+    context do
+      let(:params) { acp author_id: 1 }
+
+      before {expect(subject).to receive(:params).and_return(params) }
+
+      before { expect(Author).to receive(:find).with(params[:author_id]).and_return(:author) }
+
+      its(:author) {should eq :author}
+    end
   end
 
   describe '#resource_params' do
@@ -27,19 +49,34 @@ RSpec.describe Backoffice::AvatarsController, type: :controller do
     its(:resource_params) { should eq params.permit(:avatar) }
   end
 
-  pending '#create.json' do
-
-    let(:resource) { double }
+  describe '#create.json' do
+    let( :resource) { double }
 
     before { expect(subject).to receive(:authenticate!).and_return(true) }
 
     before { expect(subject).to receive(:authorize_resource).and_return(true) }
 
-    before{ expect(subject).to receive(:build_resource).and_return(resource) }
+    before { expect(subject).to receive(:resource).and_return(resource) }
 
-    # before { post :create, params: { id: 1 } }
+    context do
+      before { expect(resource).to receive(:valid?).and_return(true) }
 
-    it { should render_template(:create).with_status(201) }
+      before { expect(subject).to receive(:resource).and_return(resource) }
+
+      before { expect(resource).to receive(:attach_avatar).and_return(true) }
+
+      before { post :create, params: { author_id: 1 }, format: :json }
+
+      it { should render_template(:create).with_status(201) }
+    end
+
+    context do
+      before { expect(resource).to receive(:valid?).and_return(false) }
+
+      before { post :create, params: { author_id: 1 }, format: :json }
+
+      it { should render_template(:errors).with_status(422) }
+    end
   end
 
   describe '#authorize_resource' do
---
+++ spec/services/backoffice/avatar_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Backoffice::Avatar, type: :model do
+  # let(:author) { stub_model Author }
+  # subject { described_class.new author: nil, params: nil }
+
+
+  # it { should delegate_method(:as_json).to(:auth_token).allow_nil }
+end
